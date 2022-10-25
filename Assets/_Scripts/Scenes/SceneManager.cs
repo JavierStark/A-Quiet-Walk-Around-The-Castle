6e65d93889c0b0b5cc8a8d2a9f7ee7cfa330e9a0
JavierStark <javiertorralbocortes@gmail.com>
2022-10-25 19:17:47 +0200

    fix: cursor now appears and dissapears depending on the scene and the menu scene now works properly

--- Assets/_Scripts/Scenes/SceneManager.cs
+++ Assets/_Scripts/Scenes/SceneManager.cs
@@ -1,16 +1,16 @@
-using System;
 using System.Collections;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace Scenes
+namespace _Scripts.Scenes
 {
     public class SceneManager : MonoBehaviour
     {
         [Range(0, 3)] [SerializeField] private float fadeTime;
         [Range(0, 3)] [SerializeField] private float loadingTime;
+
+        [SerializeField] private Button[] menuButtons;
         private RawImage _fadeImage;
 
         private void Awake()
@@ -21,25 +21,27 @@ namespace Scenes
         private void Start()
         {
             _fadeImage = transform.GetChild(0).GetChild(0).GetComponent<RawImage>();
+            SetupScene();
         }
 
         public void ChangeScene(int index)
         {
+            DeactivateButtons();
             Debug.Log("Change");
             StartCoroutine(ChangeSceneCoroutine(index));
         }
-        
+
         private IEnumerator ChangeSceneCoroutine(int index)
         {
             yield return StartCoroutine(FadeScene(false));
             
             UnityEngine.SceneManagement.SceneManager.LoadScene(index);
-
-            yield return new WaitForSeconds(loadingTime);
             
+            yield return new WaitForSeconds(loadingTime);
+            SetupScene();
             yield return StartCoroutine(FadeScene(true));
         }
-        
+
         private IEnumerator FadeScene(bool fadeIn)
         {
             if (fadeIn)
@@ -61,7 +63,33 @@ namespace Scenes
                 }
             }
         }
-        
+
+        private void SetupScene()
+        {
+            GetSceneButtons();
+
+            SetCursor(menuButtons.Length > 0);
+        }
+
+        private void SetCursor(bool isVisible)
+        {
+            Cursor.lockState = isVisible? CursorLockMode.None : CursorLockMode.Locked;
+            Cursor.visible = isVisible;
+        }
+
+        private void GetSceneButtons()
+        {
+            menuButtons = FindObjectsOfType<Button>();
+        }
+
+        private void DeactivateButtons()
+        {
+            foreach (Button b in menuButtons)
+            {
+                b.interactable = false;
+            }
+        }
+
         private float Map( float value, float leftMin, float leftMax, float rightMin, float rightMax )
         {
             return rightMin + ( value - leftMin ) * ( rightMax - rightMin ) / ( leftMax - leftMin );
